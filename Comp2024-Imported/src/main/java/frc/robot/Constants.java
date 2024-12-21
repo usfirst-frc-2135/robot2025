f91541d66e7ed21b41d723881f26508b63eeca98
Jeff Mullins <jeff.l.mullins@gmail.com>
2024-12-20 21:56:20 -0800

    Convert AprilTag ID poses to use simpler rotation entry

--- Comp2024-Imported/src/main/java/frc/robot/Constants.java
+++ Comp2024-Imported/src/main/java/frc/robot/Constants.java
@@ -7,7 +7,6 @@ import java.util.List;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class provides a convenient place to hold robot-wide numerical or boolean
@@ -141,24 +140,24 @@ public class Constants
   public static final class VIConsts
   {
     /** Field locations (poses) of AprilTags */
-    public static final List<Pose2d> kAprilTagPoses = Collections.unmodifiableList(List.of( //
-        new Pose2d(new Translation2d(0.0, 0.0), new Rotation2d(0)),                                         // AprilTag ID: 0 (invalid)
-        new Pose2d(new Translation2d(15.079472, 0.245872), new Rotation2d(Units.degreesToRadians(120))),  // AprilTag ID: 1   - Blue source right
-        new Pose2d(new Translation2d(16.185134, 0.883666), new Rotation2d(Units.degreesToRadians(120))),  // AprilTag ID: 2   - Blue source left
-        new Pose2d(new Translation2d(16.579342, 4.982718), new Rotation2d(Units.degreesToRadians(180))),  // AprilTag ID: 3   - Red speaker right
-        new Pose2d(new Translation2d(16.579342, 5.547868), new Rotation2d(Units.degreesToRadians(180))),  // AprilTag ID: 4   - Red speaker center
-        new Pose2d(new Translation2d(14.700758, 8.2042), new Rotation2d(Units.degreesToRadians(270))),    // AprilTag ID: 5   - Red amp
-        new Pose2d(new Translation2d(1.8415, 8.20426), new Rotation2d(Units.degreesToRadians(270))),      // AprilTag ID: 6   - Blue amp
-        new Pose2d(new Translation2d(-0.0381, 5.547868), new Rotation2d(Units.degreesToRadians(0))),        // AprilTag ID: 7   - Blue speaker center
-        new Pose2d(new Translation2d(-0.0381, 4.982718), new Rotation2d(Units.degreesToRadians(0))),        // AprilTag ID: 8   - Blue speker left
-        new Pose2d(new Translation2d(0.356108, 0.883666), new Rotation2d(Units.degreesToRadians(60))),    // AprilTag ID: 9   - Red source right
-        new Pose2d(new Translation2d(1.461516, 0.245872), new Rotation2d(Units.degreesToRadians(60))),    // AprilTag ID: 10  - Red source left
-        new Pose2d(new Translation2d(11.904726, 3.713226), new Rotation2d(Units.degreesToRadians(300))),  // AprilTag ID: 11  - Red stage left
-        new Pose2d(new Translation2d(11.904726, 4.49834), new Rotation2d(Units.degreesToRadians(60))),    // AprilTag ID: 12  - Red stage right
-        new Pose2d(new Translation2d(11.220196, 4.105148), new Rotation2d(Units.degreesToRadians(180))),  // AprilTag ID: 13  - Red stage center
-        new Pose2d(new Translation2d(5.320792, 4.105148), new Rotation2d(Units.degreesToRadians(0))),     // AprilTag ID: 14  - Blue stage center
-        new Pose2d(new Translation2d(4.641342, 4.49834), new Rotation2d(Units.degreesToRadians(120))),    // AprilTag ID: 15  - Blue stage left
-        new Pose2d(new Translation2d(4.641342, 3.713226), new Rotation2d(Units.degreesToRadians(240)))    // AprilTag ID: 16  - Blue stage right
+    public static final List<Pose2d> kAprilTagPoses = Collections.unmodifiableList(List.of(           //
+        new Pose2d(new Translation2d(0.0, 0.0), Rotation2d.fromDegrees(0)),               // AprilTag ID: 0 (invalid)
+        new Pose2d(new Translation2d(15.079472, 0.245872), Rotation2d.fromDegrees(120)),  // AprilTag ID: 1   - Blue source right
+        new Pose2d(new Translation2d(16.185134, 0.883666), Rotation2d.fromDegrees(120)),  // AprilTag ID: 2   - Blue source left
+        new Pose2d(new Translation2d(16.579342, 4.982718), Rotation2d.fromDegrees(180)),  // AprilTag ID: 3   - Red speaker right
+        new Pose2d(new Translation2d(16.579342, 5.547868), Rotation2d.fromDegrees(180)),  // AprilTag ID: 4   - Red speaker center
+        new Pose2d(new Translation2d(14.700758, 8.2042), Rotation2d.fromDegrees(270)),    // AprilTag ID: 5   - Red amp
+        new Pose2d(new Translation2d(1.8415, 8.20426), Rotation2d.fromDegrees(270)),      // AprilTag ID: 6   - Blue amp
+        new Pose2d(new Translation2d(-0.0381, 5.547868), Rotation2d.fromDegrees(0)),        // AprilTag ID: 7   - Blue speaker center
+        new Pose2d(new Translation2d(-0.0381, 4.982718), Rotation2d.fromDegrees(0)),        // AprilTag ID: 8   - Blue speker left
+        new Pose2d(new Translation2d(0.356108, 0.883666), Rotation2d.fromDegrees(60)),    // AprilTag ID: 9   - Red source right
+        new Pose2d(new Translation2d(1.461516, 0.245872), Rotation2d.fromDegrees(60)),    // AprilTag ID: 10  - Red source left
+        new Pose2d(new Translation2d(11.904726, 3.713226), Rotation2d.fromDegrees(300)),  // AprilTag ID: 11  - Red stage left
+        new Pose2d(new Translation2d(11.904726, 4.49834), Rotation2d.fromDegrees(60)),    // AprilTag ID: 12  - Red stage right
+        new Pose2d(new Translation2d(11.220196, 4.105148), Rotation2d.fromDegrees(180)),  // AprilTag ID: 13  - Red stage center
+        new Pose2d(new Translation2d(5.320792, 4.105148), Rotation2d.fromDegrees(0)),     // AprilTag ID: 14  - Blue stage center
+        new Pose2d(new Translation2d(4.641342, 4.49834), Rotation2d.fromDegrees(120)),    // AprilTag ID: 15  - Blue stage left
+        new Pose2d(new Translation2d(4.641342, 3.713226), Rotation2d.fromDegrees(240))    // AprilTag ID: 16  - Blue stage right
     ));
 
     /** Destination field poses for the robot when using PathPlanner pathfinding */
