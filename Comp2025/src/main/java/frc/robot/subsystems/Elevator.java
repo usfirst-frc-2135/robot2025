c7e87e7730361cab501e32778ec238e144f1f314
Thien-Hong N <tnguyen.27@presentationhs.org>
2025-02-06 16:37:22 -0800

    Organized existing imports

--- Comp2025/src/main/java/frc/robot/subsystems/Elevator.java
+++ Comp2025/src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,7 +4,6 @@
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Amps;
-// import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.Volts;
 
