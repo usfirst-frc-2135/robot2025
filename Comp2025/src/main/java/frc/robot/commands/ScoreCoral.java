5c9f4459bbc4d9056d1cd0768431bb02181688e2
Thien-Hong N <tnguyen.27@presentationhs.org>
2025-02-19 15:09:08 -0800

    Unused Seconds Import removed

--- Comp2025/src/main/java/frc/robot/commands/ScoreCoral.java
+++ Comp2025/src/main/java/frc/robot/commands/ScoreCoral.java
@@ -1,8 +1,6 @@
 
 package frc.robot.commands;
 
-import static edu.wpi.first.units.Units.Seconds;
-
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants;
