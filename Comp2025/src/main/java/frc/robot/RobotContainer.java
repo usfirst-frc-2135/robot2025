99f8a64ebe5cf4d6747acd5e09b559c3d8395b61
Ananya K <akidambi.26@presentationhs.org>
2025-03-02 14:05:25 -0800

    Added mechanism parameters for autos

--- Comp2025/src/main/java/frc/robot/RobotContainer.java
+++ Comp2025/src/main/java/frc/robot/RobotContainer.java
@@ -558,13 +558,13 @@ public class RobotContainer
         m_autoCommand = new AutoPreload(ppPathList, m_drivetrain, m_elevator, m_manipulator, m_led, m_hid);
         break;
       case AUTOPRELOADCORAL :
-        m_autoCommand = new AutoPreloadCoral(ppPathList, m_drivetrain);
+        m_autoCommand = new AutoPreloadCoral(ppPathList, m_drivetrain, m_elevator, m_manipulator, m_led, m_hid);
         break;
       case AUTOPRELOADCORAL2 :
-        m_autoCommand = new AutoPreloadCoral2(ppPathList, m_drivetrain);
+        m_autoCommand = new AutoPreloadCoral2(ppPathList, m_drivetrain, m_elevator, m_manipulator, m_led, m_hid);
         break;
       case AUTOPRELOADCORAL3 :
-        m_autoCommand = new AutoPreloadCoral3(ppPathList, m_drivetrain);
+        m_autoCommand = new AutoPreloadCoral3(ppPathList, m_drivetrain, m_elevator, m_manipulator, m_led, m_hid);
         break;
       case AUTOTEST :
         // m_autoCommand = new AutoTest(ppPathList, m_drivetrain, m_led);
