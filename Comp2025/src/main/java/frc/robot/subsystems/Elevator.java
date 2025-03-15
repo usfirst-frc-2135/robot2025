dba8f140d49ee972b55c4e682f07ce86977cf0eb
dnanduri <dnanduri.27@presentationhs.org>
2025-03-15 15:08:21 -0700

    Moved PreloadCoral control point and updated L4 Elevator Height

--- Comp2025/src/main/java/frc/robot/subsystems/Elevator.java
+++ Comp2025/src/main/java/frc/robot/subsystems/Elevator.java
@@ -87,7 +87,7 @@ public class Elevator extends SubsystemBase
   private static final double  kHeightCoralL1          = 3.0;             // By definition - at L1 for scoring coral
   private static final double  kHeightCoralL2          = 8.25;             // By definition - at L2 for scoring coral
   private static final double  kHeightCoralL3          = 15.5;            // By definition - at L3 for scoring coral
-  private static final double  kHeightCoralL4          = 27.95;           // By definition - at L4 for scoring coral
+  private static final double  kHeightCoralL4          = 29.0;           // By definition - at L4 for scoring coral
 
   private static final double  kHeightAlgaeL23         = 11.0;            // By definition - at L23 for taking algae
   private static final double  kHeightAlgaeL34         = 19.5;            // By definition - at L34 for taking algae
