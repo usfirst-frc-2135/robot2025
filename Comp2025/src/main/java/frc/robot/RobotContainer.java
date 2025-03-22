0b3c9c4a609ffcdf8e67859d200ad14032b0ee58
Jeff Mullins <jmullins16@users.noreply.github.com>
2025-03-22 11:17:38 -0700

    Update RobotContainer.java
    Removed last reference to m_led

--- Comp2025/src/main/java/frc/robot/RobotContainer.java
+++ Comp2025/src/main/java/frc/robot/RobotContainer.java
@@ -112,7 +112,6 @@ public class RobotContainer
 
   // The robot's shared subsystems
   private final HID                                   m_hid           = new HID(m_driverPad.getHID( ), m_operatorPad.getHID( ));
-  //private final LED                                   m_led           = new LED( );
   private final Power                                 m_power         = new Power( );
   private final Vision                                m_vision        = new Vision( );
 
