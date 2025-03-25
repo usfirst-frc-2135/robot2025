ac59941fb93d6013805f0b6d8c6722b355e4c296
Jeff Mullins <jeff.l.mullins@gmail.com>
2025-03-24 22:09:14 -0700

    Convert swerve PID to use isFinished instead of Trigger

--- Comp2025/src/main/java/frc/robot/commands/DrivePIDCommand.java
+++ Comp2025/src/main/java/frc/robot/commands/DrivePIDCommand.java
@@ -13,23 +13,24 @@ import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.trajectory.PathPlannerTrajectoryState;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class DrivePIDCommand extends Command
 {
-  public CommandSwerveDrivetrain      mSwerve;
-  public final Pose2d                 goalPose;
+  public CommandSwerveDrivetrain      m_drivetrain;
+  public final Pose2d                 m_goalPose;
 
   public static final PIDConstants    kTranslationPID     = new PIDConstants(5.0, 0, 0);
   public static final PIDConstants    kRotationPID        = new PIDConstants(5.0, 0, 0);
@@ -43,85 +44,67 @@ public class DrivePIDCommand extends Command
 
   public static final PathConstraints kPathConstraints    = new PathConstraints(1.25, 1.25, 1 / 2 * Math.PI, 1 * Math.PI); // The constraints for this path.
 
-  private final Trigger               endTrigger;
-  private final Trigger               endTriggerDebounced;
+  private final Debouncer             endDebouncer        =
+      new Debouncer(kEndTriggerDebounce.in(Seconds), Debouncer.DebounceType.kBoth);
+  private final BooleanPublisher      endConditionLogger  =
+      NetworkTableInstance.getDefault( ).getTable("logging").getBooleanTopic("SwervePIDEndCondition").publish( );
 
-  private final BooleanPublisher      endTriggerLogger    =
-      NetworkTableInstance.getDefault( ).getTable("logging").getBooleanTopic("PositionPIDEndTrigger").publish( );
+  private boolean                     endCondition        = false;
 
-  public DrivePIDCommand(CommandSwerveDrivetrain mSwerve, Pose2d goalPose)
+  public DrivePIDCommand(CommandSwerveDrivetrain drivetrain, Pose2d goalPose)
   {
-    this.mSwerve = mSwerve;
-    this.goalPose = goalPose;
+    this.m_drivetrain = drivetrain;
+    this.m_goalPose = goalPose;
 
     setName("DrivePIDCommand");
-
-    DataLogManager.log(String.format("%s: constructor ....................", getName( )));
-
-    endTrigger = new Trigger(( ) ->
-    {
-      Pose2d diff = mSwerve.getState( ).Pose.relativeTo(goalPose);
-
-      var rotation = MathUtil.isNear(0.0, diff.getRotation( ).getRotations( ), kRotationTolerance.getRotations( ), 0.0, 1.0);
-
-      var position = diff.getTranslation( ).getNorm( ) < kPositionTolerance.in(Meters);
-
-      var speed = mSwerve.getState( ).Speeds.vxMetersPerSecond < kSpeedTolerance.in(MetersPerSecond)
-          && mSwerve.getState( ).Speeds.vyMetersPerSecond < kSpeedTolerance.in(MetersPerSecond);
-
-      DataLogManager.log(String.format("%s: end trigger conditions R: %s P: %s S: %s", getName( ), rotation, position, speed));
-
-      return rotation && position && speed;
-    });
-
-    endTriggerDebounced = endTrigger.debounce(kEndTriggerDebounce.in(Seconds));
   }
 
-  public static Command generateCommand(CommandSwerveDrivetrain swerve, Pose2d goalPose, Time timeout)
+  public static Command generateCommand(CommandSwerveDrivetrain swerve, Pose2d m_goalPose, Time timeout)
   {
-    DataLogManager.log(String.format("DrivePIDCommand: generateCommand ...................."));
-    return new DrivePIDCommand(swerve, goalPose).withTimeout(timeout)
+    return new DrivePIDCommand(swerve, m_goalPose).withTimeout(timeout)
         .finallyDo(( ) -> swerve.setControl(new SwerveRequest.SwerveDriveBrake( )));
   }
 
   @Override
   public void initialize( )
-  {
-    DataLogManager.log(String.format("%s: initialize ....................", getName( )));
-
-    endTriggerLogger.accept(endTrigger.getAsBoolean( ));
-  }
+  {}
 
   @Override
   public void execute( )
   {
     PathPlannerTrajectoryState goalState = new PathPlannerTrajectoryState( );
-    goalState.pose = goalPose;
+    goalState.pose = m_goalPose;
 
-    // DataLogManager.log(String.format("%s: execute ....................", getName( )));
+    ChassisSpeeds speeds = mDriveController.calculateRobotRelativeSpeeds(m_drivetrain.getState( ).Pose, goalState);
 
-    endTriggerLogger.accept(endTrigger.getAsBoolean( ));
-
-    ChassisSpeeds speeds = mDriveController.calculateRobotRelativeSpeeds(mSwerve.getState( ).Pose, goalState);
-
-    DataLogManager.log(String.format("%s: execute speed %s", getName( ), speeds));
-
-    mSwerve.setControl(new SwerveRequest.ApplyRobotSpeeds( ).withSpeeds(speeds));
+    m_drivetrain.setControl(new SwerveRequest.ApplyRobotSpeeds( ).withSpeeds(speeds));
   }
 
   @Override
   public void end(boolean interrupted)
   {
-    DataLogManager
-        .log(String.format("%s: interrupted end trigger conditions G: %s P: %s", getName( ), goalPose, mSwerve.getState( ).Pose));
-
-    endTriggerLogger.accept(endTrigger.getAsBoolean( ));
+    DataLogManager.log(String.format("%s: interrupted end trigger conditions G: %s P: %s", getName( ), m_goalPose,
+        m_drivetrain.getState( ).Pose));
   }
 
   @Override
   public boolean isFinished( )
   {
-    DataLogManager.log(String.format("%s: isFinished ....................", getName( )));
-    return endTriggerDebounced.getAsBoolean( );
+    Pose2d diff = m_drivetrain.getState( ).Pose.relativeTo(m_goalPose);
+
+    boolean rotation = MathUtil.isNear(0.0, diff.getRotation( ).getRotations( ), kRotationTolerance.getRotations( ), 0.0, 1.0);
+
+    boolean position = diff.getTranslation( ).getNorm( ) < kPositionTolerance.in(Meters);
+
+    boolean speed = m_drivetrain.getState( ).Speeds.vxMetersPerSecond < kSpeedTolerance.in(MetersPerSecond)
+        && m_drivetrain.getState( ).Speeds.vyMetersPerSecond < kSpeedTolerance.in(MetersPerSecond);
+
+    DataLogManager.log(String.format("%s: end trigger conditions R: %s P: %s S: %s", getName( ), rotation, position, speed));
+
+    endCondition = endDebouncer.calculate(rotation && position && speed);
+
+    endConditionLogger.accept(endCondition);
+
+    return endCondition;
   }
 }
