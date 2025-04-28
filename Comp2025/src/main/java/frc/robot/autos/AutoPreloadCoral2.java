2ad4964d664e1c031f09b606b0e18e65db4791cc
Jeff Mullins <jeff.l.mullins@gmail.com>
2025-04-27 23:07:35 -0700

    Remove unnecessary HID rumble steps from AutoPreloadCoral2 to save time

--- Comp2025/src/main/java/frc/robot/autos/AutoPreloadCoral2.java
+++ Comp2025/src/main/java/frc/robot/autos/AutoPreloadCoral2.java
@@ -1,23 +1,16 @@
 package frc.robot.autos;
 
-import static edu.wpi.first.units.Units.Seconds;
-
 import java.util.List;
 import java.util.function.Supplier;
 
 import com.pathplanner.lib.path.PathPlannerPath;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.Constants;
 import frc.robot.Constants.CRConsts.ClawMode;
-import frc.robot.Constants.ELConsts;
-import frc.robot.Constants.ELConsts.ReefLevel;
 import frc.robot.commands.LogCommand;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Elevator;
@@ -67,9 +60,9 @@ public class AutoPreloadCoral2 extends SequentialCommandGroup
           drivetrain.getPathCommand(ppPaths.get(0)),
           new SequentialCommandGroup(
             new LogCommand(getName(),"Move Manipulator to safe position"),
-            manipulator.getMoveToPositionCommand(ClawMode.CORALMAINTAIN, manipulator:: getAngleSafeState), 
-            new LogCommand(getName(),"Move Elevator to L3 height to save time"),
-            elevator.getMoveToPositionCommand(elevator::getHeightCoralL3)
+            manipulator.getMoveToPositionCommand(ClawMode.CORALMAINTAIN, manipulator:: getAngleSafeState)
+            // new LogCommand(getName(),"Move Elevator to L3 height to save time"),
+            // elevator.getMoveToPositionCommand(elevator::getHeightCoralL3)
           )
         ),
 
@@ -84,7 +77,7 @@ public class AutoPreloadCoral2 extends SequentialCommandGroup
 
         new LogCommand(getName( ), "Wait for coral to expel"), 
         new WaitUntilCommand(manipulator::isCoralExpelled).withTimeout(0.3), // Coral takes about 0.250 to pass through sensor plus some extra margin
-        new WaitCommand(0.1),                                                 // Wait just a little longer to ensure it completely exits
+        new WaitCommand(0.050),                                                 // Wait just a little longer to ensure it completely exits
 
         new LogCommand(getName( ), "Stop coral rollers"),
         manipulator.getMoveToPositionCommand(ClawMode.STOP, manipulator::getAngleSafeState),
@@ -105,9 +98,7 @@ public class AutoPreloadCoral2 extends SequentialCommandGroup
             new WaitUntilCommand(manipulator::isCoralDetected),
           
             new LogCommand(getName(), "Stop coral rollers move to safe state for next Elevator raise"),
-            manipulator.getMoveToPositionCommand(ClawMode.STOP, manipulator:: getCurrentAngle), // TODO: needed with CANRange?
-            hid.getHIDRumbleDriverCommand(Constants.kRumbleOn, Seconds.of(1.0), Constants.kRumbleIntensity),
-            hid.getHIDRumbleOperatorCommand(Constants.kRumbleOn, Seconds.of(1.0), Constants.kRumbleIntensity)     
+            manipulator.getMoveToPositionCommand(ClawMode.STOP, manipulator:: getCurrentAngle)
           )
         ), 
 
@@ -135,7 +126,7 @@ public class AutoPreloadCoral2 extends SequentialCommandGroup
 
         new LogCommand(getName( ), "Wait for coral to expel"), 
         new WaitUntilCommand(manipulator::isCoralExpelled).withTimeout(0.3), // Coral takes about 0.250 to pass through sensor plus some extra margin
-        new WaitCommand(0.1),                                                 // Wait just a little longer to ensure it completely exits
+        new WaitCommand(0.050),                                                 // Wait just a little longer to ensure it completely exits
 
         new LogCommand(getName( ), "Stop coral rollers"),
         manipulator.getMoveToPositionCommand(ClawMode.STOP, manipulator::getAngleSafeState),
@@ -156,9 +147,7 @@ public class AutoPreloadCoral2 extends SequentialCommandGroup
             new WaitUntilCommand(manipulator::isCoralDetected),
           
             new LogCommand(getName(), "Stop coral rollers move to safe state for next Elevator raise"),
-            manipulator.getMoveToPositionCommand(ClawMode.STOP, manipulator:: getCurrentAngle),  // TODO: needed with CANRange?
-            hid.getHIDRumbleDriverCommand(Constants.kRumbleOn, Seconds.of(1.0), Constants.kRumbleIntensity),
-            hid.getHIDRumbleOperatorCommand(Constants.kRumbleOn, Seconds.of(1.0), Constants.kRumbleIntensity)     
+            manipulator.getMoveToPositionCommand(ClawMode.STOP, manipulator:: getCurrentAngle)
           )
         ), 
 
@@ -186,7 +175,7 @@ public class AutoPreloadCoral2 extends SequentialCommandGroup
 
         new LogCommand(getName( ), "Wait for coral to expel"), 
         new WaitUntilCommand(manipulator::isCoralExpelled).withTimeout(0.3), // Coral takes about 0.250 to pass through sensor plus some extra margin
-        new WaitCommand(0.1),                                                 // Wait just a little longer to ensure it completely exits
+        new WaitCommand(0.050),                                                 // Wait just a little longer to ensure it completely exits
 
         new LogCommand(getName( ), "Stop coral rollers"),
         manipulator.getMoveToPositionCommand(ClawMode.STOP, manipulator::getAngleSafeState),
